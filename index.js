b69cb83b9b7effa4fc140195b51821d00621d4ef
Janet Webster <twixmixy@gmail.com>
2023-10-21 15:55:25 -0400

    generateLogo and init functions created

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const createShape = require('./lib/createShape.js');
 const createFile = "./examples/logo.svg";
 
 // function to generate logo
+function generateLogo(response) {
+    const svgImage = createShape(response);
+    fs.writeFile(createFile, svgImage, () => console.log(`Your new logo has been generated as "logo.svg"`))
+}
 
 // function to initiate the questions
+function init() {
+    inquirer.createPromptModule(questions)
+    .then((response) => {
+        generateLogo(response);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
 
 // call init
+init();
