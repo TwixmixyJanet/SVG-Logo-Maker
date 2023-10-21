e5707b0c9a8cc0be9b92ac613ced03268f3cc55e
Janet Webster <twixmixy@gmail.com>
2023-10-21 13:24:11 -0400

    laying out shape framework

---
+++ index.js
@@ -0,0 +1,12 @@
+// imports and connections
+const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions.js');
+const createShape = require('./lib/createShape.js');
+const createFile = "./examples/logo.svg";
+
+// function to generate logo
+
+// function to initiate the questions
+
+// call init
