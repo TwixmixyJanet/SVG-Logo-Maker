44859704bfff71d71931b2862ca18d28fd6b15d1
Janet Webster <twixmixy@gmail.com>
2023-10-22 19:04:51 -0400

    Added comments to the files

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-// imports and connections
+// IMPORTS & MODULES //
+// inquirer used for the questions
 const inquirer = require('inquirer');
+// fs for writeFile
 const fs = require('fs');
+// connecting the questions module
 const questions = require('./lib/questions');
+// connecting the createShape
 const createShape = require('./lib/createShape.js');
+// file path for making the logo file
 const createFile = "./examples/logo.svg";
+// added to make the text fun colors!
 const chalk = require('chalk');
 
 // function to generate logo
 function generateLogo(response) {
+    // calling on the create shape module
     const svgImage = createShape(response);
+    // writing the file to generate the logo // also using chalk within the template literal to make the text fun colors.
     fs.writeFile(createFile, svgImage, () => console.log(`
     ${chalk.magentaBright('~~~~~~~~~~~ Your new logo has been generated as "logo.svg" ~~~~~~~~~~~')}
     ${chalk.cyanBright('~~~~~~~~~~~~ This file can be found in the examples folder ~~~~~~~~~~~~')}
@@ -24,15 +32,18 @@ function init() {
         ${chalk.magentaBright('~~~~~~~~~~ Welcome to your logo generator! ~~~~~~~~~~')}
         ${chalk.cyanBright('~~~~~~~~~~~~~ Follow the prompts below. ~~~~~~~~~~~~~')}
         `),
+    // tapping into the inquierer import capability and the questions module
     inquirer
     .prompt(questions)
+    // after answering the questions, initiating the generateLogo function
     .then((response) => {
         generateLogo(response);
     })
+    // to catch any errors
     .catch(err => {
         console.log(err)
     });
 }
 
-// call init
+// call init function
 init();
